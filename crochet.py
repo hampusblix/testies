c0374bee6c99a6ec16212fe55a47616cfdc03d8a
Hampus <hampus.blix@gmail.com>
2025-11-03 10:08:35 +0100

    Add a new variable for merging attempt knob

--- crochet.py
+++ crochet.py
@@ -2,3 +2,4 @@ monkey_dialogue = 'me eat orange me give orange'
 monkey_epoxy = 'a glue made from ground up monkey bones'
 #this will be important later in the game
 monkey_epoxy_uses = ['seal a leaky gut', 'proof a dough', 'treat floorboards']
+whatever = 'try to merge this ya knob'
